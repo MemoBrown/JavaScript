9dbaa53919216c7195270a5a888084c7225ba940
Guillermo Moreno <contact@memobrown.com>
2020-02-10 17:27:04 -0600

    nuevo ejercicio

--- javascript-profesional/assets/index.js
+++ javascript-profesional/assets/index.js
@@ -5,7 +5,9 @@ const video = document.querySelector('video');
 const play_v = document.getElementById('play-video');
 const pause_v = document.getElementById('pause-video');
 
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
+const player = new MediaPlayer({ el: video, plugins: [
+  //new AutoPlay()
+] });
 play_v.onclick = () => player.play();
 pause_v.onclick = () => player.pause();
 
